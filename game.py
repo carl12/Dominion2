e4a5e8fe3fbd71bbeda39a0bee27dbcc95d76c89
carl12 <carl.rodriguez13@gmail.com>
2017-10-02 23:27:09 -0700

    Play around with some simulations

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from store import Store
 from manager import *
+import random
 class Game:
 	
 	def __init__(self, managers, store):
@@ -22,42 +23,73 @@ class Game:
 		turns = 1000
 		winners = []
 		for i,m in enumerate(self.managers):
-			if m.get_points() > maxM:
+			points = m.get_points()
+			if points > maxM:
 				winners = [i]
-				maxM = m.get_points()
+				maxM = points
 				turns = m.turns
-			elif m.get_points == maxM:
+			elif points == maxM:
 				if m.turns < turns:
 					winners = [i]
 				elif m.turns == turns:
-					winner.append(i)
+					winners.append(i)
 		return winners
+
+	def get_stats(self):
+		stats = {}
+		stats['winner'] = self.get_winner()
+		stats['points'] = [m.get_points() for m in self.managers]
+		stats['turns'] = [m.turns for m in self.managers]
+		stats['cardList'] = self.get_all()
+		return stats
+
+
+	def get_all(self):
+		a = [m.get_all() for m in self.managers]
+		c = sum([1 if card.name == 'Province' else 0  for card in a[0]])
+		d = sum([1 if card.name == 'Province' else 0  for card in a[1]])
+		cardList = [[card.name for card in player] for player in a]
+		return cardList
+
+	def count_vp(self):
+		pass
+		
 
 def make_game():
 	store = Store(2)
-	p1 = DumbMoney(store)
-	p2 = DumbMoneyV3(store)
-	return Game([p1,p2],store)
+	p1 = DumbMoneyV3(store)
+	p2 = DumbMoneyV2(store)
+	if random.choice([True, False]):
+		return (Game([p1,p2],store),True)
+	else:
+		return (Game([p2,p1],store),False)
+	
 
 def play_game():
-	g = make_game()
+	g,order = make_game()
 	g.play_turn(100)
-	winner = g.get_winner()
+	stats = g.get_stats()
+	winner = stats['winner']
 	if len(winner) == 1:
-		return winner[0]
+		if order:
+			return winner[0]
+		else:
+			return 0 if winner[0] else 1
 	
 def play_100():
 	win = [0,0]
-	for _ in range(1000):
+	for i in range(10000):
+		if (i+1)%1000==0:
+			print(i+1,'games')
 		winner = play_game()
-		win[winner]+=1
-		print(winner)
+		if winner is not None:
+			win[winner]+=1
 	print(win)
 		
 
-
-
 play_100()
+
+
 # g = make_game()
 # g.play_turn(100)
 # print(g.managers[0].player.set.all)
